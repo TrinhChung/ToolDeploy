1da799c1f5912956cedb050e0a26b9abf1aa14ea
TrinhChung <chungtrinh2k2@gmail.com>
2025-07-24 21:30:02 +0900

    change port

--- app.py
+++ app.py
@@ -111,4 +111,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         seed_admin_user(app)
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=1904, debug=False)
